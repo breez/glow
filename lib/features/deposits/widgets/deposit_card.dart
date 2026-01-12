7df1b7e09ff042042aa9101661df4ae5b1bfa3ef
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2026-01-12 16:32:56 +0300

    remove unused import

--- lib/features/deposits/widgets/deposit_card.dart
+++ lib/features/deposits/widgets/deposit_card.dart
@@ -1,6 +1,5 @@
 import 'package:breez_sdk_spark_flutter/breez_sdk_spark.dart';
 import 'package:flutter/material.dart';
-import 'package:glow/features/deposits/widgets/deposit_error_banner.dart';
 import 'package:glow/widgets/warning_box.dart';
 
 /// Individual deposit card widget - Pure UI component
